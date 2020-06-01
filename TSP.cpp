7749015d15656eef13b7b3aab288f287450531bb
yaxenia <49765435+yaxenia@users.noreply.github.com>
2020-06-01 20:20:40 +0300

    Create TSP.cpp

---
+++ TSP.cpp
@@ -0,0 +1,84 @@
+// Traveling Salesman Problem
+//The undirected weighted graph is given by the adjacency matrix. Find the shortest cycle that visits each vertex one time.
+using namespace  std;
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+vector<vector<int>> Getmatrix(int& n)
+{
+    int a;
+    vector<vector<int>> m;
+    for (int i = 0 ; i < n; i++) {
+        vector<int> t;
+        for (int j = 0; j < n; j++) {
+            cin >> a;
+            t.push_back(a);
+
+        }
+        m.push_back(t);
+
+    }
+    return m;
+}
+
+int TSP(const vector<vector<int> >& graph, int n)
+{
+
+
+    vector<int> vert;
+    bool index = false;
+
+
+    if (n == 1)
+    {
+        return 0;
+    }
+    for (int i = 0; i < n; i++)
+        vert.push_back(i);
+
+    int path = INT_MAX;
+
+    do
+    {
+        int k = vert[0];
+        vert.push_back(k);
+        int distance = 0;
+
+        for (int i = 1; i < vert.size(); i++)
+        {
+            if (graph[k][vert[i]] == 0)
+            {
+                index = true;
+                break;
+            }
+
+            distance += graph[k][vert[i]];
+            k = vert[i];
+        }
+
+        if (index == false)
+            path = min(path, distance);
+        vert.pop_back();
+
+    }while (next_permutation(vert.begin(), vert.end()));
+
+    if (path >= INT_MAX)
+        return -1;
+    else
+        return path;
+
+
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> graph = Getmatrix(n);
+    vector<vector<int>> vertex(graph.size());
+    cout << TSP(graph,n);
+    return 0;
+}
+
